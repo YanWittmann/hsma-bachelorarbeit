ca75e8ce8997f4bbbc27c1dc72ca0be76f14d7c2
ywittmann <yan.wittmann@metaeffekt.com>
2025-07-17 18:22:27 +0200

    Vollständiges neuformulieren der Anforderungen und Hinzufügen meiner Unterschrift

--- latex/create.sh
+++ latex/create.sh
@@ -1,44 +1,47 @@
 #!/bin/sh
-RESULT_DIR="../result"
-FILE="thesis"
-ENGINE="pdflatex"
-LAST_ENGINE_FILE=".last_engine"
+clear
+time (
+    RESULT_DIR="../result"
+    FILE="thesis"
+    ENGINE="pdflatex"
+    LAST_ENGINE_FILE=".last_engine"
 
-if [ "$1" = "lua" ]; then
-    ENGINE="lualatex"
-fi
+    if [ "$1" = "lua" ]; then
+        ENGINE="lualatex"
+    fi
 
-if [ -f "$LAST_ENGINE_FILE" ]; then
-    LAST_ENGINE=$(cat "$LAST_ENGINE_FILE")
-else
-    LAST_ENGINE=""
-fi
+    if [ -f "$LAST_ENGINE_FILE" ]; then
+        LAST_ENGINE=$(cat "$LAST_ENGINE_FILE")
+    else
+        LAST_ENGINE=""
+    fi
 
-if [ "$ENGINE" != "$LAST_ENGINE" ]; then
-    ./clean.sh
-fi
+    if [ "$ENGINE" != "$LAST_ENGINE" ]; then
+        ./clean.sh
+    fi
 
-echo "$ENGINE" > "$LAST_ENGINE_FILE"
+    echo "$ENGINE" > "$LAST_ENGINE_FILE"
 
-cd tex
-if [ "$ENGINE" = "lualatex" ]; then
-    lualatex -synctex=1 -shell-escape --enable-write18 -draftmode $FILE
-else
-    pdflatex -synctex=1 -shell-escape --enable-write18 -draftmode $FILE
-fi
-makeindex -s $FILE.ist -t $FILE.alg -o $FILE.acr $FILE.acn
-makeindex -s $FILE.ist -t $FILE.glg -o $FILE.gls $FILE.glo
-makeindex -s $FILE.ist -t $FILE.slg -o $FILE.syi $FILE.syg
-biber $FILE
-if [ "$ENGINE" = "lualatex" ]; then
-    lualatex -synctex=1 -shell-escape --enable-write18 -draftmode $FILE > /dev/null
-    lualatex -synctex=1 -shell-escape --enable-write18 -draftmode -interaction batchmode $FILE > /dev/null
-    lualatex -synctex=1 -shell-escape --enable-write18 -interaction batchmode $FILE
-else
-    pdflatex -synctex=1 -shell-escape --enable-write18 -draftmode $FILE > /dev/null
-    pdflatex -synctex=1 -shell-escape --enable-write18 -draftmode -interaction batchmode $FILE > /dev/null
-    pdflatex -synctex=1 -shell-escape --enable-write18 -interaction batchmode $FILE
-fi
-mkdir -p $RESULT_DIR
-cat ${FILE}.pdf > $RESULT_DIR/${FILE}.pdf
-cd ..
+    cd tex
+    if [ "$ENGINE" = "lualatex" ]; then
+        lualatex -synctex=1 -shell-escape --enable-write18 -draftmode $FILE
+    else
+        pdflatex -synctex=1 -shell-escape --enable-write18 -draftmode $FILE
+    fi
+    makeindex -s $FILE.ist -t $FILE.alg -o $FILE.acr $FILE.acn
+    makeindex -s $FILE.ist -t $FILE.glg -o $FILE.gls $FILE.glo
+    makeindex -s $FILE.ist -t $FILE.slg -o $FILE.syi $FILE.syg
+    biber $FILE
+    if [ "$ENGINE" = "lualatex" ]; then
+        lualatex -synctex=1 -shell-escape --enable-write18 -draftmode $FILE > /dev/null
+        lualatex -synctex=1 -shell-escape --enable-write18 -draftmode -interaction batchmode $FILE > /dev/null
+        lualatex -synctex=1 -shell-escape --enable-write18 -interaction batchmode $FILE
+    else
+        pdflatex -synctex=1 -shell-escape --enable-write18 -draftmode $FILE > /dev/null
+        pdflatex -synctex=1 -shell-escape --enable-write18 -draftmode -interaction batchmode $FILE > /dev/null
+        pdflatex -synctex=1 -shell-escape --enable-write18 -interaction batchmode $FILE
+    fi
+    mkdir -p $RESULT_DIR
+    cat ${FILE}.pdf > $RESULT_DIR/${FILE}.pdf
+    cd ..
+)
